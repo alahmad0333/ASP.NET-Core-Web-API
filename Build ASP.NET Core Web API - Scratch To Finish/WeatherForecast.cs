79f13aa1bb6f0beecdaeb376c70cf1a94309e2af
Ahmad Al-Zahrani <az6741a@gmail.com>
2023-04-06 03:49:38 +0300

    Add project files.

---
+++ Build ASP.NET Core Web API - Scratch To Finish/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Build_ASP.NET_Core_Web_API___Scratch_To_Finish
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
